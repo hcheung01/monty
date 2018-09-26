92d4b57c5aa6168515b4cf67e8e3ca4156d0989a
Joshua Jimenez <jsjimenez51@gmail.com>
2018-09-26 21:33:12 +0000

    Created operations2 file that holds nop

---
+++ operations2.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - function that does nothing.
+ * @stk: the stack.
+ * @line_number: the line number.
+ *
+ * Return: void
+ */
+void nop(stack_t **stk, unsigned int line_number)
+{
+	(void)stk;
+	(void)line_number;
+}
+
