b6be7dbe606b20e9c0d3e43385b4964609a3f045
Joshua Jimenez <jsjimenez51@gmail.com>
2018-09-26 23:05:11 +0000

    added div functionality

--- op_maths.c
+++ op_maths.c
@@ -3,51 +3,77 @@
 /**
  * add - add the top two elements of the stack.
  * @stk: the stack.
- * @line_num: the linenumber.
+ * @line_number: the linenumber.
  *
  * Return: void
  */
-void add(stack_t **stk, unsigned int line_num)
+void add(stack_t **stk, unsigned int line_number)
 {
 	int sum;
 
 	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
 	{
-		Error /* need to add to Error function */;
-		free_stk(stk, line_num);
+		printf("L%d: can't add, stack too short\n", line_number);
+		free_stk(stk, line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	sum = variables.temp;
-	pop(stk, line_num);
+	pop(stk, line_number);
 	sum += variables.temp;
-	pop(stk, line_num);
+	pop(stk, line_number);
 	variables.temp = sum;
-	push(stk, line_num);
+	push(stk, line_number);
 }
 
 /**
  * sub - subtracts the top two elements of the stack.
  * @stk: the stack.
- * @line_num: the line number.
+ * @line_number: the line number.
  *
  * Return: void.
  */
-void sub(stack_t **stk, unsigned int line_num)
+void sub(stack_t **stk, unsigned int line_number)
 {
 	int diff;
 
 	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
 	{
-		printf("L%d: can't subtract, stack is too small\n", line_num);
-		free_stk(stk, line_num);
+		printf("L%d: can't subtract, stack too short\n", line_number);
+		free_stk(stk, line_number);
 		exit(EXIT_FAILURE);\
 	}
 
 	diff = variables.temp;
-	pop(stk, line_num);
+	pop(stk, line_number);
 	diff = variables.temp - diff;
-	pop(stk, line_num);
+	pop(stk, line_number);
 	variables.temp = diff;
-	push(stk, line_num);
+	push(stk, line_number);
+}
+
+void _div(stack_t **stk, unsigned int line_number)
+{
+	int quote;
+
+	if (stk == NULL || *stk == NULL || (*stk)->next == NULL)
+	{
+		printf("L%d: can't div, stack too short\n", line_number);
+		free_stk(stk, line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (variables.temp == 0)
+	{
+		printf("L%d: division by zero\n", line_number);
+		free_stk(stk, line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	quote = variables.temp;
+	pop(stk, line_number);
+	quote = variables.temp / quote;
+	pop(stk, line_number);
+	variables.temp = quote;
+	push(stk, line_number);
 }
