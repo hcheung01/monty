bf2b1ac59d71ec7f41a40b56a183b584f2d4fe58
Heindrick Cheung <hcheung01@gmail.com>
2018-09-26 20:05:17 +0000

    update

--- monty.c
+++ monty.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include "lists.h"
 
-char **split_line(char *line);
+char **tokenizer(char *line);
 void readfile(FILE *filename);
+void *findOps(char *token, int l_n);
+void print_All(void);
 
 void main(int argc, char *argv[])
 {
@@ -38,21 +40,19 @@ void readfile(FILE *filename)
 	int line_number;
 
 	line_number = 0;
-	while (getline(&buf, &bufsize, filename) != -1)
+	while (getline(&buf, &bufsize, filename) != EOF)
 	{
-		tokens = split_line(buf);
+		tokens = tokenizer(buf);
 		line_number++;
-		printf("line number is: %d\n", line_number);
 	}
 	free(buf);
 }
 
-char **split_line(char *line)
+char **tokenizer(char *line)
 {
 	size_t count = 0;
 	char **tokens, *token;
 
-	printf("PASSING LINE ARGUMENT IS: %s\n", line);
 	tokens = malloc(sizeof(char *) * 3);
 	if (tokens == NULL)
 	{
@@ -61,20 +61,43 @@ char **split_line(char *line)
 	}
 
 	token = strtok(line, DELIM);
-	printf("TOKEN1 IS: %s\n", token);
 	if (token == NULL)
 	{
 		free(tokens);
 		return (NULL);
 	}
-	while (token != NULL)
+	printf("TOKEN = %s\n", token);
+	while (token != NULL && count < 2)
 	{
+//		findOps(token, count);
 		tokens[count] = token;
 		token = strtok(NULL, DELIM);
-		printf("TOKEN2 IS: %s\n", token);
+		printf("TOKEN = %s\n", token);
 		count++;
 	}
 	tokens[count] = NULL;
 	return (tokens);
 }
+/*
+void *findOps(char *token, int l_n)
+{
+	int i;
 
+	printf("PASSED TOKEN IS: %s\n", token);
+        printf("PASSED LINE NO IS: %d\n", l_n);
+	instruction_t all_func[] = {
+		{"PUSH", print_All},
+		{NULL, NULL}
+	};
+
+	for(i = 0; all_func[i].token != NULL; i++)
+	{
+		if (strcmp(token, all_func[i].[0]) == 0)
+			print_All();
+			}
+}
+
+void print_All(void)
+{
+	printf("FOUND PUSH\n");
+	}*/
