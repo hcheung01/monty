b86100c45ba3be1820d0dce8c63161b029da7a98
Joshua Jimenez <jsjimenez51@gmail.com>
2018-09-27 01:10:47 +0000

    uploaded split_str.c file

---
+++ split_str.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+
+/**
+ * tokenizer - function with 3 arguments
+ * @input: buffer input
+ * @stk: stack argument
+ * @line_number: line count
+ *
+ * Description: break up the buffer into tokens
+ * Return: na
+ */
+void tokenizer(char *input, stack_t **stk, unsigned int line_number)
+{
+        char *token;
+        char *tokens;
+
+        token = strtok(input, " ");
+        if (token == NULL || *token == ' ' || *token == '\n')
+                return;
+        if (strcmp(token, "push") == 0)
+	{
+                tokens = token;
+                token = strtok(NULL, " ");
+                if (!is_num(token))
+                {
+                        printf("L%d: usage: push integer\n", line_number);
+                        free_stk(stk, line_number);
+                        exit(EXIT_FAILURE);
+                }
+                variables.temp = atoi(token);
+                findOps(tokens, stk, line_number);
+        }
+        else
+                findOps(token, stk, line_number);
+}
+
+/**
+ *  is_num - checks if the string is a number
+ * @token: the string being checked.
+ *
+ * Return: 1 if number, 0 if not.
+ */
+int is_num(char *token)
+{
+        if (token == NULL)
+                return (0);
+        if (*token == '-')
+                token++;
+        while (*token != '\0')
+        {
+                if (!isdigit(*token))
+                        return (0);
+                token++;
+        }
+        token++;
+        return (1);
+}
