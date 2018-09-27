6c6b79f4c6e4f3e20b0ce9091d8a35258f7bc5f6
Heindrick Cheung <hcheung01@gmail.com>
2018-09-27 01:00:39 +0000

    updated header

--- monty.h
+++ monty.h
@@ -75,6 +75,8 @@ void sub(stack_t **stk, unsigned int line_number);
 
 void _div(stack_t **stk, unsigned int line_number);
 
+void _mul(stack_t **stk, unsigned int line_number);
+
 int is_num(char *token);
 
 #endif
