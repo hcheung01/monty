20dcb027051bc9f1af5b48ccb523bb7cbbed10dc
Heindrick Cheung <hcheung01@gmail.com>
2018-09-28 00:19:26 +0000

    update

--- op_maths.c
+++ op_maths.c
@@ -111,7 +111,7 @@ void _mul(stack_t **stk, unsigned int line_number)
 
 	product = variables.temp;
 	pop(stk, line_number);
-	product *= variables.temp;
+	product = variables.temp * product;
 	pop(stk, line_number);
 	variables.temp = product;
 	push(stk, line_number);
@@ -146,7 +146,7 @@ void _mod(stack_t **stk, unsigned int line_number)
 
 	modulus = variables.temp;
 	pop(stk, line_number);
-	modulus %= variables.temp;
+	modulus = variables.temp % modulus;
 	pop(stk, line_number);
 	variables.temp = modulus;
 	push(stk, line_number);
